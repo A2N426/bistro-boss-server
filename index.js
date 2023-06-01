3dea57f9e2c3311ef363e5de5d43d0f22c2e20af
Mohammad Apon <umar426.net@gmail.com>
2023-06-01 21:11:32 +0600

    manageItems and add items and delete and update operations done

--- index.js
+++ index.js
@@ -11,15 +11,15 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const verifyJWT = (req,res,next)=>{
+const verifyJWT = (req, res, next) => {
     const authorization = req.headers.authorization;
-    if(!authorization){
-        return res.status(401).send({error:true,message:"unauthorized access"})
+    if (!authorization) {
+        return res.status(401).send({ error: true, message: "unauthorized access" })
     }
     const token = authorization.split(" ")[1];
-    jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err,decoded)=>{
-        if(err){
-            return res.status(401).send({error:true,message:"unauthorized access"})
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(401).send({ error: true, message: "unauthorized access" })
         }
         req.decoded = decoded;
         next();
@@ -55,26 +55,26 @@ async function run() {
         const cartCollection = client.db("bossDB").collection("carts");
 
         // jwt token collect
-        app.post("/jwt",(req,res)=>{
+        app.post("/jwt", (req, res) => {
             const user = req.body;
-            const token = jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{expiresIn:"1h"});
-            res.send({token})
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "1h" });
+            res.send({ token })
         })
 
         // verify admin
-        const verifyAdmin = async(req,res,next)=>{
+        const verifyAdmin = async (req, res, next) => {
             const email = req.decoded.email;
-            const query = {email:email}
+            const query = { email: email }
             const user = await userCollection.findOne(query);
-            if(user.role !== "admin"){
-                return res.status(403).send({error:true,message:"forbidden access"})
+            if (user.role !== "admin") {
+                return res.status(403).send({ error: true, message: "forbidden access" })
             }
             next()
         }
 
         // users related apis
-        app.get("/users",verifyJWT,verifyAdmin,async(req,res)=>{
-            const result =await userCollection.find().toArray();
+        app.get("/users", verifyJWT, verifyAdmin, async (req, res) => {
+            const result = await userCollection.find().toArray();
             res.send(result)
         })
 
@@ -93,28 +93,28 @@ async function run() {
         // security layer: verifyJWT
         // email same
         // check admin 
-        app.get("/users/admin/:email",verifyJWT,async (req,res)=>{
+        app.get("/users/admin/:email", verifyJWT, async (req, res) => {
             const email = req.params.email;
 
-            if(req.decoded.email !== email){
-                res.send({admin:false})
+            if (req.decoded.email !== email) {
+                res.send({ admin: false })
             }
 
-            const query = {email : email};
+            const query = { email: email };
             const user = await userCollection.findOne(query);
-            const result = {admin: user?.role === "admin"};
+            const result = { admin: user?.role === "admin" };
             res.send(result)
         })
 
-        app.patch("/users/admin/:id",async(req,res)=>{
+        app.patch("/users/admin/:id", async (req, res) => {
             const id = req.params.id;
-            const filter = {_id : new ObjectId(id)}; 
+            const filter = { _id: new ObjectId(id) };
             const updatedDoc = {
-                $set:{
-                    role:"admin"
+                $set: {
+                    role: "admin"
                 }
             }
-            const result = await userCollection.updateOne(filter,updatedDoc);
+            const result = await userCollection.updateOne(filter, updatedDoc);
             res.send(result)
         })
 
@@ -122,6 +122,19 @@ async function run() {
         app.get("/menu", async (req, res) => {
             const result = await menuCollection.find().toArray()
             res.send(result)
+        })
+
+        app.post("/menu", verifyJWT, verifyAdmin, async (req, res) => {
+            const item = req.body;
+            const result = await menuCollection.insertOne(item);
+            res.send(result);
+        })
+
+        app.delete("/menu/:id", verifyJWT , verifyAdmin ,async(req,res)=>{
+            const id = req.params.id;
+            const query = {_id : new ObjectId(id)};
+            const result = await menuCollection.deleteOne(query);
+            res.send(result);
         })
 
         // reviews related apis
@@ -132,15 +145,15 @@ async function run() {
 
         // cart data apis
 
-        app.get("/carts", verifyJWT , async (req, res) => {
+        app.get("/carts", verifyJWT, async (req, res) => {
             const email = req.query.email;
             if (!email) {
                 return res.send([])
             }
 
             const decoded = req.decoded.email;
-            if(email !== decoded){
-                return res.status(403).send({error:true,message:"forbidden access"})
+            if (email !== decoded) {
+                return res.status(403).send({ error: true, message: "forbidden access" })
             }
 
             const query = { email: email };
